277a88ce70eb75499136fa3b5fde390d7d0860a4
EoghanONeill <oneill.eoghan.1@gmail.com>
2021-11-12 13:56:35 +0100

    Added R wrapper functions for the fast C++ Test_aSPA, Test_uSPA, and MCS Test functions.

---
+++ R/Fast_Test_aSPA.R
@@ -0,0 +1,59 @@
+#' Fast Test average Superior Predictive Ability
+#' 
+#' Implements the test for average Superior Predictive Ability (aSPA) of Quaedvlieg (2021)
+#' @param LossDiff the T x H matrix forecast path loss differential
+#' @param weights the 1 x H vector of weights for the losses at different horizons. For instance \code{weights <- matlab::ones(1,20)/20}
+#' @param L integer, the parameter for the moving block bootstrap
+#' @param B integer, the number of bootstrap iterations. Default 999
+#' @return A list containing two objects:
+#' \item{"p_value"}{the p-value for aSPA}
+#' \item{"t_aSPA"}{the statistics for aSPA}
+#' @references Quaedvlieg, Rogier. "Multi-horizon forecast comparison." Journal of Business & Economic Statistics 39.1 (2021): 40-53.
+#' @seealso \code{\link{Test_uSPA}}
+#' @examples
+#' ## Test for aSPA and uSPA
+#' data(LossDiff_aSPA)
+#' weights <- matlab::ones(1,20)/20
+#' Fast_Test_aSPA(LossDiff=LossDiff_aSPA, weights=weights, L=3, B=10)
+#' 
+#' @author Luca Barbaglia \url{https://lucabarbaglia.github.io/}
+#' 
+#' @export
+
+Fast_Test_aSPA <- function(LossDiff, 
+                           weights = NULL, 
+                           L, 
+                           B=999){
+  
+  
+  if (!is.matrix(LossDiff)){LossDiff <- as.matrix(LossDiff)}
+  
+  Hcols <- ncol(LossDiff)
+  
+  Mmethods <- length(Losses)
+  
+  if(is.null(weights)){
+    weights <- rep(1/Hcols, Hcols)
+  }
+  if(length(weights)!=Hcols){
+    print("length of weights not equal to number of columns (horizons), setting equal weights")
+    weights <- rep(1/Hcols, Hcols)
+  }
+  
+  
+  ret <- Test_aSPA_cpp(as.matrix(LossDiff),
+                       weights,
+                       L, 
+                       B)
+  
+  # bootout <- Bootstrap_uSPA(LossDiff, L, B)
+  # p_value <- mean(bootout$t_uSPA < bootout$t_uSPA_b)
+  # 
+  
+  
+  
+  return(list("p_value"=ret[[0]], "t_aSPA"=ret[[1]]))
+  
+  
+}
+
---
+++ R/Fast_Test_uSPA.R
@@ -0,0 +1,42 @@
+#' Fast Test uniform Superior Predictive Ability
+#' 
+#' Implements the test for uniform Superior Predictive Ability (uSPA) of Quaedvlieg (2021)
+#' @param LossDiff the T x H matrix forecast path loss differential
+#' @param L the parameter for the moving block bootstrap
+#' @param B integer, the number of bootstrap iterations. Default 999
+#' @return A list containing two objects:
+#' \item{"p_value"}{the p-value for uSPA}
+#' \item{"t_uSPA"}{the statistics for uSPA}
+#' @references Quaedvlieg, Rogier. "Multi-horizon forecast comparison." Journal of Business & Economic Statistics 39.1 (2021): 40-53.
+#' @seealso \code{\link{Test_aSPA}}
+#' @examples
+#' ## Test for uSPA
+#' data(LossDiff_uSPA)
+#' Fast_Test_uSPA(LossDiff=LossDiff_uSPA, L=3, B=10)
+#' 
+#' @author Luca Barbaglia \url{https://lucabarbaglia.github.io/}
+#' 
+#' @export
+
+Fast_Test_uSPA <- function(LossDiff, 
+                           L, 
+                           B=999){
+  
+  
+  if (!is.matrix(LossDiff)){LossDiff <- as.matrix(LossDiff)}
+  
+  ret <- Test_uSPA_cpp(as.matrix(LossDiff), 
+                       L, 
+                       B)
+  
+  # bootout <- Bootstrap_uSPA(LossDiff, L, B)
+  # p_value <- mean(bootout$t_uSPA < bootout$t_uSPA_b)
+  # 
+  
+  
+  
+  return(list("p_value"=ret[[0]], "t_uSPA"=ret[[1]]))
+  
+  
+}
+
